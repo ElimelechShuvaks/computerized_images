2c30768920871ff5bd133eb2336b9fedebc1f069
gittiroz@gmail.com <gittiroz@gmail.com>
2023-03-02 11:36:54 +0200

    i add the Triangle class

---
+++ src/geometries/Triangle.java
@@ -0,0 +1,17 @@
+package geometries;
+
+import primitives.Point;
+
+/**
+ *Represents a triangular object.
+ */
+public class Triangle extends Polygon {
+
+    /**
+     *constructor.
+     * @param vertices
+     */
+    public Triangle(Point... vertices) {
+        super(vertices);
+    }
+}
