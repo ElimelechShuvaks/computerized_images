324cfc3f34d617f4c0cc4a50052cb17410107fa9
gittiroz@gmail.com <gittiroz@gmail.com>
2023-04-17 13:16:10 +0300

    I add test Vector class

---
+++ unittests/primitives/VectorTest.java
@@ -0,0 +1,39 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for Vector class.
+ */
+class VectorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+
+    @Test
+    void lengthSquared() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void normalize() {
+    }
+
+    @Test
+    void dotProduct() {
+    }
+}
